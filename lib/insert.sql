1879547065d16e66637d695866c400bd1dafce22
Marion Saru <maghangasaru@gmail.com>
2025-07-22 21:53:31 +0300

    Completed lab

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES
+  (1, 'Mr. Chocolate', 5, 'M', 'Brown', 'Sweet', 1),
+  (2, 'Rowdy', 3, 'M', 'Black', 'Aggressive', 1),
+  (3, 'Tabitha', 7, 'F', 'White', 'Calm', 1),
+  (4, 'Sergeant Brown', 6, 'M', 'Brown', 'Authoritative', 1),
+  (5, 'Melissa', 4, 'F', 'Grizzly', 'Playful', 1),
+  (6, 'Grinch', 2, 'M', 'Greenish', 'Grumpy', 1),
+  (7, 'Wendy', 9, 'F', 'Blonde', 'Gentle', 1),
+  (8, NULL, 1, 'F', 'Black', 'Curious', 1); -- "unnamed" bear
